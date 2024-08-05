31cbe1d57babc62b88d9fd2aa17e35dc3d20e83e
Moe Yan Htun <moeyan227970@gmail.com>
2024-08-05 23:25:30 +0630

    Revert "Merge pull request #1 from moeyanhtun-coding/feature"
    This reverts commit 53e1a675af778010eaa842125364c2ec18dc7b14, reversing
    changes made to 398a4d63733a8a80807db8a5072b4e73212ce654.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:starman/views/home_view.dart';
 
 void main() {
   runApp(const HelloWorldApp());
@@ -11,8 +10,13 @@ class HelloWorldApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeView(),
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello World App'),
+      ),
+      body: const Center(
+        child: Text('Hello, World!!!'),
+      ),
+    ));
   }
 }
