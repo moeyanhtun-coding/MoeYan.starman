edb943a458cfe4844a1bb24030be93b64caf8f5b
Moe Yan Htun <moeyan227970@gmail.com>
2024-08-05 22:49:40 +0630

    [Add] Alert DialogBox

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -10,25 +10,63 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        drawer: NavBar(),
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Home Page'),
-          backgroundColor: Colors.grey[600],
-        ),
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _remainingBox();
+    });
+  }
 
-        //body
-        body: const Center(
-          child: Text(
-            'Home Page',
-            style: TextStyle(fontSize: 30.0),
-          ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Home Page'),
+        backgroundColor: Colors.grey[600],
+      ),
+
+      //body
+      body: const Center(
+        child: Text(
+          'Home Page',
+          style: TextStyle(fontSize: 30.0),
         ),
       ),
     );
+  }
+
+  Future _remainingBox() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Color.fromARGB(255, 255, 243, 243),
+            title: Row(
+              children: [
+                Icon(
+                  Icons.circle_notifications,
+                  color: Colors.redAccent,
+                  size: 30,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('Day Remaining'),
+              ],
+            ),
+            content: Text('This is a dialog box.'),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Close'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
   }
 }
