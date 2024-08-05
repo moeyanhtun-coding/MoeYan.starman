059ad580ca00aad951f2e7a36655ad51588d0170
Moe Yan Htun <moeyan227970@gmail.com>
2024-08-05 22:49:29 +0630

    [Add] Material App

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:starman/views/home_view.dart';
 
 void main() {
-  runApp(const HomeView());
+  runApp(const HelloWorldApp());
+}
+
+class HelloWorldApp extends StatelessWidget {
+  const HelloWorldApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
+  }
 }
