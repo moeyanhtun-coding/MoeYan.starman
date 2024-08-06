3288ab76fad455111a9dbb992a606b4dad8e04a0
Moe Yan Htun <moeyan227970@gmail.com>
2024-08-06 16:23:40 +0630

    [Add] Api Configure

--- lib/network/fusion_api.dart
+++ lib/network/fusion_api.dart
@@ -1,32 +1,123 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'dart:developer' as dev;
+import '../models/last_subscription_model/last_subscription_model.dart';
 import '../models/star_group_model/star_group_model.dart';
+import '../models/star_links_model/star_links_model.dart';
+import '../models/star_subscriptions_model/star_subscriptions_model.dart';
 
-class FusionApi {
-  String username = 'fusion_dev';
-  String password = 'fusion_dev';
-  Future<StarGroupModel> getStarGroup() async {
-    try {
-      String basicAuth =
-          'Basic ${base64.encode(utf8.encode('$username:$password'))}';
-      dev.log(basicAuth);
-      var queryParameters = {'starId': '957a-562D'};
-      var url = Uri.https('api.fusionmyanmar.com', '/rest/starman/getStarGroup',
-          queryParameters);
-      final response = await http.post(url, headers: <String, String>{
+//* Common PostRequestAPI *//
+Future<http.Response> _makePostRequest({
+  required String endpoint,
+  required Map<String, String> queryParams,
+  required String username,
+  required String password,
+}) async {
+  try {
+    String basicAuth =
+        'Basic ${base64.encode(utf8.encode('$username:$password'))}';
+    var url = Uri.https('api.fusionmyanmar.com', endpoint, queryParams);
+    final response = await http.post(
+      url,
+      headers: {
         'Authorization': basicAuth,
         'SECRET_ACCESS_TOKEN':
-            "\$2a\$10\$wl2BjK4NHQwB6npW2xOWCOyFN/x3s92TKnLdSDFSVnTCuxIDg8mVG"
-      });
+            "\$2a\$10\$wl2BjK4NHQwB6npW2xOWCOyFN/x3s92TKnLdSDFSVnTCuxIDg8mVG",
+      },
+    );
 
-      if (response.statusCode == 200) {
-        return StarGroupModel.fromJson(json.decode(response.body));
-      } else {
-        throw Exception("Result Fail");
-      }
+    if (response.statusCode == 200) {
+      return response;
+    } else {
+      throw Exception("Request failed with status: ${response.statusCode}");
+    }
+  } catch (e) {
+    throw Exception("Request error: ${e.toString()}");
+  }
+}
+
+class FusionApi {
+  //* SatrGroupAPI *//
+  Future<StarGroupModel> getStarGroup(
+    String username,
+    String password,
+    String endpoint,
+    Map<String, String> queryParams,
+  ) async {
+    try {
+      final response = await _makePostRequest(
+        endpoint: endpoint,
+        queryParams: queryParams,
+        username: username, // Replace with your actual username
+        password: password, // Replace with your actual password
+      );
+      return StarGroupModel.fromJson(json.decode(response.body));
     } catch (e) {
-      throw Exception(e.toString());
+      throw Exception("Error getting star group: ${e.toString()}");
+    }
+  }
+
+//* LastSubscriptionAPI *//
+  Future<LastSubscriptionModel> getLastSubscription(
+    String username,
+    String password,
+    String endpoint,
+    Map<String, String> queryParams,
+  ) async {
+    try {
+      final response = await _makePostRequest(
+        endpoint: endpoint,
+        queryParams: queryParams,
+        username: username,
+        password: password,
+      );
+      return LastSubscriptionModel.fromJson(json.decode(response.body));
+    } catch (e) {
+      throw Exception("Error getting star group: ${e.toString()}");
+    }
+  }
+
+  //* StarLinksAPI *//
+  Future<List<StarLinksModel>> getStarLinks(
+    String username,
+    String password,
+    String endpoint,
+    Map<String, String> queryParams,
+  ) async {
+    try {
+      final response = await _makePostRequest(
+        endpoint: endpoint,
+        queryParams: queryParams,
+        username: username, // Replace with your actual username
+        password: password, // Replace with your actual password
+      );
+      List<dynamic> body = json.decode(response.body);
+      return body.map((dynamic item) => StarLinksModel.fromJson(item)).toList();
+    } catch (e) {
+      throw Exception("Error getting star group: ${e.toString()}");
+    }
+  }
+
+  //* StarSubscriptionsAPI *//
+  Future<List<StarSubscriptionsModel>> getStarSubscriptions(
+    String username,
+    String password,
+    String endpoint,
+    Map<String, String> queryParams,
+  ) async {
+    try {
+      final response = await _makePostRequest(
+        endpoint: endpoint,
+        queryParams: queryParams,
+        username: username, // Replace with your actual username
+        password: password, // Replace with your actual password
+      );
+      List<dynamic> body = json.decode(response.body);
+      return body
+          .map((dynamic item) => StarSubscriptionsModel.fromJson(item))
+          .toList();
+    } catch (e) {
+      throw Exception("Error getting star group: ${e.toString()}");
     }
   }
 }
