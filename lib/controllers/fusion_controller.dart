5040579c1a26000577e8e7e736b0b4bdd22f43bd
Moe Yan Htun <moeyan227970@gmail.com>
2024-08-06 20:34:57 +0630

    [Add] starGroup controller

--- lib/controllers/fusion_controller.dart
+++ lib/controllers/fusion_controller.dart
@@ -1,1 +1,27 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:starman/network/fusion_api.dart';
 
+class FusionController {
+  late SharedPreferences prefs;
+  FusionController() {
+    _initialize();
+  }
+  Future<void> _initialize() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  Future<void> starGroup() async {
+    await _initialize();
+    try {
+      var queryParameters = {'starId': '957a-562D'};
+      var starGroup = await FusionApi().getStarGroup(
+          'fusion_dev',
+          'fusion_dev',
+          '/rest/starman/getStarGroup',
+          queryParameters); // Assuming getStarGroup() is async
+      await prefs.setString("_starGroup", starGroup.toString());
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+}
