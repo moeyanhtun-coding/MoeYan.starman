23316e23573ad47cd10ae135c798453f95463d5d
moeyanhtun-coding <moeyan227970@gmail.com>
2024-08-11 08:23:42 +0630

    -

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,10 @@ import 'package:starman/views/starid_view.dart';
 import 'package:starman/views/passcode_view.dart';
 import 'package:starman/views/splashscreen.dart';
 
-
-class AppColors{
+class AppColors {
   static const Color primaryColor = Color(0xFFA39FD9);
   static const Color secondaryColor = Color(0xFFA6A6A6);
 }
-
 
 void main() async {
   runApp(MyApp());
@@ -25,8 +23,11 @@ class MyApp extends StatelessWidget {
     GetPage(name: '/', page: () => SplashScreen()),
     GetPage(name: '/starId', page: () => StaridView()),
     GetPage(name: '/passcode', page: () => PasscodeView()),
-    GetPage(name: '/homeView', page: () => HomeView()),
+    GetPage(name: '/home', page: () => HomeView()),
+    GetPage(name: '/error', page: () => ErrorView()),
+    GetPage(name: '/existingPasscode', page: () => ExistingPasscodeView()), GetPage(name: '/existingPasscode', page: () => ExistingPasscodeView())
   ];
+
   MyApp({super.key});
 
   @override
@@ -36,7 +37,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       getPages: routes,
       debugShowCheckedModeBanner: false,
-      home:SplashScreen(),
+      home: SplashScreen(),
     );
   }
 }
